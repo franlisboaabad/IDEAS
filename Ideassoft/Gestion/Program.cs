464b0c4a265184a5c67bdb02ddb9e56563c1595b
franlisboaabad <franco_lisboa@hotmail.com>
2018-09-06 18:13:37 -0500

    Configuracion dotnebart
    Instalacion de framework dotnebart

--- Ideassoft/Gestion/Program.cs
+++ Ideassoft/Gestion/Program.cs
@@ -18,7 +18,7 @@ namespace Gestion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Principal());
+            Application.Run(new MenuPrincipal());
         }
     }
 }
---
+++ Ideassoft/Gestion/Vistas/Login.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gestion.Vistas
+{
+    public partial class Login : DevComponents.DotNetBar.RibbonForm
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
