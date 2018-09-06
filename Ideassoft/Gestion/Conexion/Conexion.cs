4701c57736b3a098acea828ec3468f0dfba4c248
franlisboaabad <franco_lisboa@hotmail.com>
2018-09-06 16:28:48 -0500

    Configuracion
    Configuracion de proyecto inicial.

---
+++ Ideassoft/Gestion/Conexion/Conexion.cs
@@ -0,0 +1,99 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Configuration;
+using System.Windows.Forms;
+
+
+
+using System.Configuration;
+using Gestion.Class;
+
+namespace Gestion.Conexion
+{
+    class clsConexion
+    {
+
+        public clsConexion() { }
+
+        public MySqlConnection conector = null;
+        public String sConex = ConfigurationManager.ConnectionStrings["local"].ConnectionString;
+        ClsErrores error = new ClsErrores();
+
+        public MySqlConnection conectarBD()
+        {
+            try
+            {
+                conector = new MySqlConnection(sConex);
+                conector.Open();
+                return conector;
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                error.WarningError("Error al Conectar a la Base de Datos " + ex.Message);
+                throw new Exception();
+            }
+        }
+        public MySqlConnection desconectarBD()
+        {
+            try
+            {
+                conector.Close();
+                return conector;
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                error.WarningError("Error al Cerrar la Conexion " + ex.Message);
+                throw new Exception();
+            }
+        }
+
+
+        //public void GeneraraBackup(String file)
+        //{
+
+        //    try
+        //    {
+        //        MySqlConnection conn = new MySqlConnection(sConex);
+        //        MySqlCommand cmd = new MySqlCommand();
+
+        //        MySqlBackup mb = new MySqlBackup(cmd);
+        //        cmd.Connection = conn;
+        //        //conn.Open();
+        //        mb.ExportToFile(file);
+        //        conn.Close();
+        //    }
+        //    catch (MySql.Data.MySqlClient.MySqlException ex)
+        //    {
+        //        throw new Exception(ex.Message);
+        //    }
+        //}
+
+        //public void ImportarBackup(String file)
+        //{
+        //    try
+        //    {
+        //        using (MySqlConnection conn = new MySqlConnection(sConex))
+        //        {
+        //            using (MySqlCommand cmd = new MySqlCommand())
+        //            {
+        //                using (MySqlBackup mb = new MySqlBackup(cmd))
+        //                {
+        //                    cmd.Connection = conn;
+        //                    conn.Open();
+        //                    mb.ImportFromFile(file);
+        //                    conn.Close();
+        //                }
+        //            }
+        //        }
+        //        MessageBox.Show("La importación de datos se realizó con ÉXITO!!!");
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.ToString());
+        //    }
+        //}
+
+    }
+
+}
+
